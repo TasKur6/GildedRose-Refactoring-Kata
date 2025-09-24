f7826d1912a164d72c01d238365bda0b5d74e335
Tasneem Kurabadwala <Tasneem.Kurabadwala@softwire.com>
2025-09-24 14:30:36 +0100

    Aged brie increases in quality, quality capped at 50

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,9 +1,15 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
-describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+describe('Gilded Rose updateQuality', () => {
+  it('Aged Brie quality should increase', () => {
+    const gildedRose = new GildedRose([new Item('Aged Brie', 23, 30)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('foo');
+    expect(items[0].quality).toBe(31);
+  });
+  it('Quality should never go above 50', () => {
+    const gildedRose = new GildedRose([new Item('Aged Brie', 23, 50)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(50);
   });
 });
+
